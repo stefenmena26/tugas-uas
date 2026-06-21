cf1b551d1d508cafebeaa8792040a1f25a0d2615
stefenmena26 <stefanusmena90@gmail.com>
2026-06-21 23:37:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showContent(id){
+
+    const sections =
+    document.querySelectorAll('.content');
+
+    sections.forEach(section=>{
+        section.classList.remove('active');
+    });
+
+    document
+    .getElementById(id)
+    .classList.add('active');
+
+}
